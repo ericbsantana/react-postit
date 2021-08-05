4ae7007ae7a8891a1b07f9d15e6834d6cdffa39c
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 14:00:03 -0300

    "centralize" data fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,55 @@
+import { useState, useEffect } from "react";
+
 import Content from "./components/Content";
 import Sidebar from "./components/Sidebar";
 
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001";
+
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPostIts = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/fetch");
+      setData(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteCard = async (id) => {
+    try {
+      setLoading(true);
+      const response = await axios.delete("/delete", { data: { id: id } });
+      console.log(response);
+      setLoading(false);
+      fetchPostIts();
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostIts();
+  }, []);
+
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-9 gap-5">
         <div className="col-span-3 border-r border-yellow-300 ">
-          <Sidebar />
+          <Sidebar fetchPostIts={fetchPostIts} />
         </div>
         <div className="col-span-6">
-          <Content />
+          <Content
+            fetchPostIts={fetchPostIts}
+            deleteCard={deleteCard}
+            data={data}
+            loading={loading}
+          />
         </div>
       </div>
     </div>
--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,54 +1,25 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import Card from "./Card";
 
-axios.defaults.baseURL = "http://localhost:3001";
-
-const Content = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const fetchPostIts = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get("/fetch");
-      setData(response.data);
-      setLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const deleteCard = async (id) => {
-    try {
-      setLoading(true);
-      const response = await axios.delete("/delete", { data: { id: id } });
-      console.log(response);
-      setLoading(false);
-      fetchPostIts();
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchPostIts();
-  }, []);
-
+const Content = (props) => {
   return (
     <main className="p-5 space-y-5">
       <h1 className=" text-3xl text-purple-800 font-bold">Your notes</h1>
       <div className="grid grid-cols-6 gap-5">
-        {!loading &&
-          data.map((postit) => (
+        {!props.loading &&
+          props.data.map((postit) => (
             <Card
               key={postit._id}
               id={postit._id}
               title={postit.title}
               description={postit.description}
-              deleteCard={deleteCard}
+              deleteCard={props.deleteCard}
             />
           ))}
+        {props.loading && (
+          <div className="w-full flex justify-center col-span-6 text-3xl font-bold text-purple-800">
+            Loading...
+          </div>
+        )}
       </div>
     </main>
   );
