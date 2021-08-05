f312b1ea282413b20c2c14396361d343e010a9cf
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 16:20:07 -0300

    add card responsivity

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,9 +3,11 @@ import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Card = (props) => {
   return (
-    <div className="w-auto col-span-2 py-4 px-8 bg-gray-100 shadow rounded-lg border-2 border-gray-100 break-words">
+    <div className="w-full px-4 py-5 lg:w-auto lg:col-span-2 lg:px-2 lg:py- xl:py-4 xl:px-8 bg-gray-100 shadow rounded-lg border-2 border-gray-100 break-words animate-fade-in-out">
       <div className="flex justify-between items-start border-b-2 border-dotted border-gray-400 mb-2 flex-grow-2">
-        <h2 className="text-green-600 text-md font-semibold">{props.title}</h2>
+        <h2 className="text-green-600 lg:text-sm xl:text-md font-semibold">
+          {props.title}
+        </h2>
         <button
           type="button"
           onClick={() => {
@@ -13,12 +15,12 @@ const Card = (props) => {
           }}
         >
           <FontAwesomeIcon
-            className="text-purple-500 hover:text-purple-800"
+            className="text-purple-800 hover:text-purple-600"
             icon={faTimesCircle}
           />
         </button>
       </div>
-      <span className="text-gray-600">{props.description}</span>
+      <span className="text-gray-600 md:text-sm">{props.description}</span>
     </div>
   );
 };
