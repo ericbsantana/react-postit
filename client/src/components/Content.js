26e45c074fde3d033041c79a933bbaeb0e4a2cd1
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 12:02:43 -0300

    create content layout

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,5 +1,12 @@
+import Card from "./Card";
+
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <main className="p-5 space-y-5">
+      <h1 className=" text-3xl text-purple-800 font-bold">Your notes</h1>
+      <div className="grid grid-cols-6 gap-5"></div>
+    </main>
+  );
 };
 
 export default Content;
