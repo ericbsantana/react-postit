8a2a12c967a71d16d0e4c7a44d5e8bc47f205dc8
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 14:36:09 -0300

    add close button icon

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+
 const Card = (props) => {
   return (
-    <div className="col-span-2 py-4 px-8 bg-gray-100 shadow rounded-lg border-2 border-gray-100 break-words">
-      <div className="flex justify-between border-b-2 border-dotted border-gray-400 mb-2">
+    <div className="w-auto col-span-2 py-4 px-8 bg-gray-100 shadow rounded-lg border-2 border-gray-100 break-words">
+      <div className="flex justify-between items-start border-b-2 border-dotted border-gray-400 mb-2 flex-grow-2">
         <h2 className="text-green-600 text-md font-semibold">{props.title}</h2>
         <button
           type="button"
@@ -9,7 +12,10 @@ const Card = (props) => {
             props.deleteCard(props.id);
           }}
         >
-          X
+          <FontAwesomeIcon
+            className="text-purple-500 hover:text-purple-800"
+            icon={faTimesCircle}
+          />
         </button>
       </div>
       <span className="text-gray-600">{props.description}</span>
