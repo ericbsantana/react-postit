a78059fdcd7225d71555af29ff8ec866a8a7a958
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 12:54:29 -0300

    add card props and delete button

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,10 +2,17 @@ const Card = (props) => {
   return (
     <div className="col-span-2 py-4 px-8 bg-gray-100 shadow rounded-lg border-2 border-gray-100 break-words">
       <div className="flex justify-between border-b-2 border-dotted border-gray-400 mb-2">
-        <h2 class="text-green-600 text-md font-semibold">Title</h2>
-        <button className="">X</button>
+        <h2 className="text-green-600 text-md font-semibold">{props.title}</h2>
+        <button
+          type="button"
+          onClick={() => {
+            props.deleteCard(props.id);
+          }}
+        >
+          X
+        </button>
       </div>
-      <span class="text-gray-600">Description</span>
+      <span className="text-gray-600">{props.description}</span>
     </div>
   );
 };
