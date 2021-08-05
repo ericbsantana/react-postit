a5d54c28bb89feaf63c80d55603f5e4c4dfbb43a
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 16:19:01 -0300

    extend tailwind animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-out": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "fade-in-out": "fade-in-out 0.5s ease-in-out",
+      },
+    },
   },
   variants: {
     extend: {},
