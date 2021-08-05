3072837630eae3ff26dd05bd1ba36db10e0f844b
ericbsantana <bitencourteric@hotmail.com>
2021-08-05 12:02:12 -0300

    fix appjs content import

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import Content from "./components/Card";
+import Content from "./components/Content";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-9">
+      <div className="grid grid-cols-9 gap-5">
         <div className="col-span-3 border-r border-yellow-300 ">
           <Sidebar />
         </div>
-        <div className="">
+        <div className="col-span-6">
           <Content />
         </div>
       </div>
